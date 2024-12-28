923c68b764696402c2b020c2439901aa0e07d954
cullenwebber <139840023+cullenwebber@users.noreply.github.com>
2024-12-28 20:07:05 +0800

    Push

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,72 @@
-import Image from "next/image";
+"use client";
+
+import { useRef } from "react";
+import { Model } from "@/components/models/Rock-scene";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { BlendFunction } from "postprocessing";
+
+import { easing } from "maath";
+import {
+	Bloom,
+	EffectComposer,
+	Noise,
+	Scanline,
+} from "@react-three/postprocessing";
+import { Environment } from "@react-three/drei";
+
+function SpotLightWithHelper() {
+	const lightRef = useRef();
+
+	return (
+		<spotLight
+			ref={lightRef}
+			decay={0}
+			position={[0, 60, 10]}
+			angle={0.12}
+			penumbra={1}
+			intensity={0.5}
+			castShadow
+			shadow-mapSize={1024}
+			color={"#50DDFF"}
+		/>
+	);
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<div className="h-lvh w-full">
+			<Canvas className="h-lvh">
+				<color attach="background" args={["black"]} />
+				<SpotLightWithHelper />
+				<EffectComposer disableNormalPass>
+					<Bloom
+						luminanceThreshold={0.6}
+						mipmapBlur
+						luminanceSmoothing={0}
+						intensity={0.6}
+					/>
+					<Noise opacity={0.6} blendFunction={BlendFunction.SOFT_LIGHT} />
+					<Scanline opacity={0.1} blendFunction={BlendFunction.SOFT_LIGHT} />
+				</EffectComposer>
+				<Model />
+				<CameraRig />
+			</Canvas>
+		</div>
+	);
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+function CameraRig() {
+	useFrame((state, delta) => {
+		easing.damp3(
+			state.camera.position,
+			[
+				-1 + (state.pointer.x * state.viewport.width) / 6,
+				(3 + state.pointer.y) / 2,
+				6,
+			],
+			0.25,
+			delta
+		);
+		state.camera.lookAt(0, 2, 0);
+	});
 }
